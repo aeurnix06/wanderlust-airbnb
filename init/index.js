82de8846f76fc07b3992633170d4c885b2431928
aeurnix06 <hamaurnix@gmail.com>
2026-06-14 21:23:05 +0530

    updated

---
+++ init/index.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js")
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main().then(() => {
+    console.log("connected to DB");
+}).catch(err => {
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+};
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+    const data = initData.data.map((obj) => ({
+        ...obj,
+        image: obj.image.url,  
+    }));
+    await Listing.insertMany(data);
+    console.log("data was initialized");
+};
+
+initDB();
+
---
+++ models/listing.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const review = require("./review");
+const { ref } = require("joi");
+const Schema = mongoose.Schema;
+const Review = require("./review.js");
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        type: String,
+        default: "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?w=1200",
+        set: (v) => {
+            if (typeof v === "object" && v.url) return v.url;
+            if (v === "") return "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?w=1200";
+            return v;
+        },
+    },
+    price: Number,
+    location: String,
+    country: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review",
+        },
+    ],
+});
+
+listingSchema.post("findOneAndDelete", async(listing) =>{
+    if(listing) {
+        await Review.deleteMany({_id: {$in: listing.reviews}});
+    }
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
