23fd379712031191486a81cf94fa4fda48a3187d
aeurnix06 <hamaurnix@gmail.com>
2026-06-17 08:57:00 +0530

    Added authentication and middleware

--- routes/listings.js
+++ routes/listings.js
@@ -4,6 +4,7 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const { listingSchema, reviewSchema } = require("../schema.js");
 const Listing = require("../models/listing.js");
+const {isLoggedIn} = require("../middleware.js");
 
 
 const validateListing = (req, res, next) => {
@@ -25,7 +26,7 @@ router.get("/", wrapAsync(async (req, res) => {
 
 
 //New Route
-router.get("/new", (req, res) => {
+router.get("/new", isLoggedIn, (req, res) => {
     res.render("listings/new.ejs");
 });
 
@@ -44,7 +45,7 @@ router.get("/:id", wrapAsync(async (req, res) => {
 
 // Create Route
 router.post(
-    "/", validateListing,
+    "/",isLoggedIn, validateListing,
     wrapAsync(async (req, res, next) => {
         const newListing = new Listing(req.body.listing);
         await newListing.save();
@@ -55,7 +56,7 @@ router.post(
 
 
 // Edit Route
-router.get("/:id/edit", wrapAsync(async (req, res) => {
+router.get("/:id/edit", isLoggedIn, wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     if(!listing)  {
@@ -67,7 +68,7 @@ router.get("/:id/edit", wrapAsync(async (req, res) => {
 
 
 // Update Route
-router.put("/:id", validateListing, wrapAsync(async (req, res) => {
+router.put("/:id",isLoggedIn, validateListing, wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     req.flash("success", "Listing Updated!");
@@ -77,7 +78,7 @@ router.put("/:id", validateListing, wrapAsync(async (req, res) => {
 
 
 // Delete Route
-router.delete("/:id", wrapAsync(async (req, res) => {
+router.delete("/:id",isLoggedIn, wrapAsync(async (req, res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
--- routes/user.js
+++ routes/user.js
@@ -17,7 +17,7 @@ router.post("/signup", wrapAsync(async (req, res) => {
         req.flash("success", "welcome to Wanderlust");
         res.redirect("/listings");
     }catch(err){
-        req.flash("error", e.message);
+        req.flash("error", err.message);
         res.redirect("/signup");
     }
 }));
@@ -34,5 +34,25 @@ router.post("/login", passport.authenticate("local", {
         req.flash("success", "Welcome back to Wanderlust!");
         res.redirect("/listings");
     });
+
+    router.get("/logout", (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        req.flash("success", "Logged out!");
+        res.redirect("/listings");
+    });
+});
+
+    router.get("/logout", (req, res, next) =>{
+        req.logout((err) =>{
+            if(err){
+                return next(err);
+            }
+            req.flash("success", "you are logged out!");
+            res.redirect("/listings");
+        })
+    })
 
 module.exports = router;
